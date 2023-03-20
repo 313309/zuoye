964e21e7254ff094a716ef6a2c9b7b65d0c0ebe0
313309 <127114412+313309@users.noreply.github.com>
2023-03-20 16:29:40 +0800

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,89 +1,44 @@
-﻿using ConsoleApplication3delegate;
-
-namespace ConsoleApplication3delegate
+﻿public class Student : IComparable
 {
-    class DepositCard
+    public double mygrade;
+    public string mysubject, myID;
+    public Student() { }
+    public Student(double grade, string subject, string id)
     {
-        public int amount;
-
-        public void Display()
+        mygrade = grade;
+        mysubject = subject;
+        myID = id;
+    }
+   int IComparable.CompareTo(object obj)
+    {
+        if (obj is Student)
         {
-            Console.WriteLine("储蓄卡余额为：{0}", amount);
+            Student stu = (Student)obj;      //stu是取的一个object的值
+            if (this.mygrade > stu.mygrade) return -1;
+            else if (this.mygrade < stu.mygrade) return 1;
+            else return 0;
         }
-        public void Account(int balance,int payday )
+        throw new ArgumentException("object is not a Student");
+
+    }
+}
+
+internal class Program
+{
+
+    private static void Main(string[] args)
+    {
+        Student[] students = new Student[5];
+        students[0] = new Student(50.0, "windows程序设计", "001");
+        students[1] = new Student(90.0, "windows程序设计", "002");
+        students[2] = new Student(100.0, "windows程序设计", "003");
+        students[3] = new Student(70.0, "windows程序设计", "004");
+        students[4] = new Student(80.0, "windows程序设计", "005");
+        Array.Sort(students);
+        foreach (Student s in students)
         {
-            amount += balance;
-            Console.WriteLine("今天是本月的{0}号，取款{1}元，储蓄卡余额为：{2}元。", DateTime.Today.Day, balance, amount);
+            Console.WriteLine(s.mygrade + " " + s.mysubject + " " + s.myID);
         }
     }
 
-    class CreditCard
-    {
-        private int billamount;
-        private int repaymentday;
-
-        public CreditCard(int billamount, int repaymentday)
-        {
-            this.billamount = billamount;
-            this.repaymentday = repaymentday;
-        }
-
-        public int Getbillamount() { return billamount; }
-
-        public int Getrepaymentday() { return repaymentday; }
-
-        public void Display() { Console.WriteLine("信用卡余额为：{0}", billamount); }
-
-
-    }
-
-    class CreditCardDelegate
-    {
-        
-        public delegate void DelegatePay(int bill, int day);
-        public event DelegatePay EventPay;
-        public int billamount;
-        public int repaymentday;
-        public void Notify()
-        {
-            Console.WriteLine("今天是{0}年{1}月{2}号", DateTime.Today.Year, DateTime.Today.Month, DateTime.Today.Day);
-            // 如果事件不为 null
-            if (DateTime.Today.Day == repaymentday)
-            {
-                EventPay(billamount, repaymentday);
-                // 触发事件
-                Console.WriteLine("还款成功！");
-            }
-            else Console.WriteLine("未到还款日，无需还款！");
-        }
-
-    }
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            DepositCard depositCard = new DepositCard();
-            depositCard.amount = 10000;
-            CreditCard creditCard1 = new CreditCard(-2000, 6);
-            CreditCard creditCard2 = new CreditCard(-3000, 13);
-            CreditCard creditCard3 = new CreditCard(-5000, 29);
-            depositCard.Display(); Console.WriteLine("");
-            List<CreditCard> Cards = new List<CreditCard>();
-            Cards.Add(creditCard1); Cards.Add(creditCard2); Cards.Add(creditCard3);
-
-            foreach (CreditCard card in Cards)
-            {
-                Console.WriteLine("信用卡开始执行委托还款。。。。。。");
-                CreditCardDelegate delegate1 = new CreditCardDelegate();
-                delegate1.billamount = card.Getbillamount();
-                delegate1.repaymentday = card.Getrepaymentday();
-                delegate1.EventPay += new CreditCardDelegate.DelegatePay(depositCard.Account);
-                delegate1.Notify();
-                depositCard.Display();
-                Console.WriteLine("");
-            }
-
-            Console.ReadLine();
-        }
-    }
 }
